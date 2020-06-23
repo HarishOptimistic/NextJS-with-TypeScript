64110e46b78f3d25b647369e7142a588640c4997
Harish Radhakrishnan <harish@haplabz.com>
2020-06-23 21:29:59 +0530

    EOD

--- src/pages/[vehicle]/[person].js
+++ src/pages/[vehicle]/[person].js
@@ -7,7 +7,7 @@ return <pre>{JSON.stringify(ownerList, null, 4)}</pre>
 
 Person.getInitialProps = async (ctx) => {
  const {query} = ctx;
- const response = await fetch('https://jsonplaceholder.typicode.com/todos');
+ const response = await fetch('https://jsonplaceholder.typicode.com/todos?completed='+ query.person);
  const ownerList = await response.json();
  return {ownerList: ownerList}
 }
