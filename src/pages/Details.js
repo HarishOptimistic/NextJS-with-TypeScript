a002d62dd59766f7a6b235cc5d7e78c9518bfb70
Harish Radhakrishnan <harish@haplabz.com>
2020-06-23 19:04:50 +0530

    End of Router Part

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,9 +1,19 @@
 import Link from 'next/link';
 
+const people = [
+ { v: 'car', name: 'Harish' },
+ { v: 'bike', name: 'John' },
+ { v: 'airplane', name: 'Alice' }
+]
+
 export default function Details() {
  return <div>
-  <Link as="/car/harish" href="/[vehicle]/[person]">
-   <a>Navigate to Harish's car</a>
-  </Link>
-  </div>
+  {people.map(e => (
+   <div>
+   <Link as={`/${e.v}/${e.name}`} href="/[vehicle]/[person]">
+    <a>Navigate to {e.name}'s {e.v}</a>
+   </Link>
+   </div>
+  ))}
+ </div>
 }
