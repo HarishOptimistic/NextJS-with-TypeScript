5c8bb8aad3f11782e585e101eb1e885109361b0a
Harish Radhakrishnan <harish@haplabz.com>
2020-06-23 18:34:59 +0530

    Dynamic Routes with variables

--- src/pages/[vehicle]/[person].js
+++ src/pages/[vehicle]/[person].js
@@ -1,3 +1,7 @@
+import { useRouter } from 'next/router';
+
 export default function Person() {
- return <h2>Harish's car</h2>
+ const router = useRouter();
+ console.log(router.query);
+return <h2>{router.query.person}'s {router.query.vehicle}</h2>
 }
