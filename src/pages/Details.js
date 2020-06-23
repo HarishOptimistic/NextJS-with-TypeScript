4c8ceaff9df45a816eae3dd5c0e4ac8e5a22d37c
Harish Radhakrishnan <harish@haplabz.com>
2020-06-23 18:47:00 +0530

    Dynamic Routes - as/href

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,3 +1,9 @@
-export default function Index() {
- return <h1>Details</h1>
+import Link from 'next/link';
+
+export default function Details() {
+ return <div>
+  <Link as="/car/harish" href="/[vehicle]/[person]">
+   <a>Navigate to Harish's car</a>
+  </Link>
+  </div>
 }
