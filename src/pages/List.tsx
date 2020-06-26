c7a682c3a28c4bbe3886d855a3c8a770fb181fd3
Harish Radhakrishnan <harish@haplabz.com>
2020-06-26 21:38:29 +0530

    converted to Typescript

--- src/pages/List.tsx
+++ src/pages/List.tsx
@@ -7,10 +7,14 @@ const owners = [
  { vehicle: 'airplane', ownerName: 'Alice' }
 ]
 
-export default function List(ownersList) {
+export interface ListProps {
+ ownersList: undefined;
+}
+
+export default function List({ownersList}: ListProps) {
 
  return (<div>
-  {owners.map((e, index) => (
+  {owners?.map((e, index) => (
    <div>
    <Link as={`/${e.vehicle}/${e.ownerName}`} href="/[vehicle]/[person]">
   <a>Navigate to {e.ownerName}'s {e.vehicle}</a>
--- src/pages/[vehicle]/[person].tsx
+++ src/pages/[vehicle]/[person].tsx
@@ -1,12 +1,19 @@
 import { useRouter } from 'next/router';
+import { NextPageContext } from 'next';
 
-export default function Person(ownerList) {
+export default function Person(ownerList: {}) {
  const router = useRouter();
 return <pre>{JSON.stringify(ownerList, null, 4)}</pre>
 }
 
-Person.getInitialProps = async (ctx) => {
- const {query} = ctx;
+interface MyNextPageContext extends NextPageContext {
+ query: {
+  person: string;
+  vehicle: string;
+ }
+}
+
+Person.getInitialProps = async ({query, req}: MyNextPageContext) => {
  const response = await fetch('https://jsonplaceholder.typicode.com/todos?completed='+ query.person);
  const ownerList = await response.json();
  return {ownerList: ownerList}
